63aab515e0186a4229b822dbe1742eb2cd151535
GBarbosaML <116645318+GBarbosaML@users.noreply.github.com>
2022-10-26 13:38:29 -0300

    Update entrada e saída.java

--- entrada e saída.java
+++ entrada e saída.java
@@ -7,7 +7,11 @@ public class EstudosTwo {
     public static void main(String[]args){
         
         Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("digite o nome aluno:");
         String nome = teclado.nextLine();
+        
+        System.out.println("digite a nota do aluno:");
         Float nota = teclado.nextFloat();
         
         System.out.print("a nota do "+nome+ " é "+ nota);
